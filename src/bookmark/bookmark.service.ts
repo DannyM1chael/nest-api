90535a4f5f4f4dfbf0cd59114a8e2eb2535258b0
Denis Mikhalev <denis.mikhalev@promwad.com>
2023-08-16 18:15:48 +0300

    fix: update gitignore

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BookmarkService {
+  getBookmarks() {}
+  getBookmarksById() {}
+  createBookmark() {}
+  editBookmarkById() {}
+  deleteBookmarkById() {}
+}
